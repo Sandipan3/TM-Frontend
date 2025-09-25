1b76d29d89b977842933133b1cddb40ca64a072d
SANDIPAN JHA <sandipanjha3@gmail.com>
2025-09-25 13:05:44 +0530

    Feat: VitePWA added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,39 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Task-Management",
+        short_name: "TM",
+        icons: [
+          {
+            src: "/maskable_icon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: ".",
+      },
+    }),
+  ],
 });
